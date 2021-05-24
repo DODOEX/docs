c46fc3f3af26aaf9ff07e6352620734bd161f1cf
牛涛涛 <15700059289@163.com>
2021-05-24 17:18:54 +0800

    modify dashbord desc

--- sidebars.js
+++ sidebars.js
@@ -1,35 +1,35 @@
 module.exports = {
-  someSidebar: {
-    "DODO V2 Intro": ["briefIntro"],
-    Protocol: ["pmm", "pmmDetails", "math"],
-    "Use Case": ["publicPool", "privatePool", "crowdPooling", "upCrowdPooling"],
-    Contract: [
-      "framework",
-      "contractUseGuide",
-      "flashSwap",
-      "deployedInfo",
-      "deployedInfoBsc",
-      "deployedInfoHeco",
-      "deployedInfoMatic",
-      "subgraph",
-      "bugBounty",
-      "audit",
-      "dppForMM"
-    ],
-    Api: ["tradeApi"],
-    BusinessData: [
-        "businessDataDashbord"
-    ],
-    Tokenomics: ["dodonomics"],
-    "DODO NFT Intro": ["nftCoreFeatures"],
-    "DODO Dojo": [
-      "ThisIsDODO",
-      "DODO-Economics-102",
-      "DODO-On-Chain-Liquidity",
-      "DODO-User-Friendly-Decentralization",
-    ],
-    FAQ : ["commonQuestions"],
-    Media: ["media"]
-    // Docusaurus: ["doc1", "doc2", "doc3", "mdx"],
-  },
+    someSidebar: {
+        "DODO V2 Intro": ["briefIntro"],
+        Protocol: ["pmm", "pmmDetails", "math"],
+        "Use Case": ["publicPool", "privatePool", "crowdPooling", "upCrowdPooling"],
+        Contract: [
+            "framework",
+            "contractUseGuide",
+            "flashSwap",
+            "deployedInfo",
+            "deployedInfoBsc",
+            "deployedInfoHeco",
+            "deployedInfoMatic",
+            "subgraph",
+            "bugBounty",
+            "audit",
+            "dppForMM"
+        ],
+        Api: ["tradeApi"],
+        "Business Data": [
+            "businessDataDashbord"
+        ],
+        Tokenomics: ["dodonomics"],
+        "DODO NFT Intro": ["nftCoreFeatures"],
+        "DODO Dojo": [
+            "ThisIsDODO",
+            "DODO-Economics-102",
+            "DODO-On-Chain-Liquidity",
+            "DODO-User-Friendly-Decentralization",
+        ],
+        FAQ: ["commonQuestions"],
+        Media: ["media"]
+        // Docusaurus: ["doc1", "doc2", "doc3", "mdx"],
+    },
 };
